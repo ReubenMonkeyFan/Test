0a135285b9da9d6fa0b298150461006db86271f6
swan.reube <swan.reube@awatapu.school.nz>
2021-07-06 14:18:21 +1200

    I watched the video and repeated the shown code

---
+++ 00_sandbox.py
@@ -0,0 +1,11 @@
+#ask user for a number
+get_answer = int(input("Choose a number"))
+
+
+#multiply number by five
+times_five = get_answer * 5
+
+answer = "{} times five is equal to {}".format(get_answer, times_five)
+
+#output the result
+print(answer)
